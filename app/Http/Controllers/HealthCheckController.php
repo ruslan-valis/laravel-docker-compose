a587df5f89e7e26117d17501c21eacec1bc2c072
Ruslan Valis <ruslan.valis@gmail.com>
2024-04-30 17:08:44 +0100

    Add X-Owner validation

--- app/Http/Controllers/HealthCheckController.php
+++ app/Http/Controllers/HealthCheckController.php
@@ -15,6 +15,14 @@ class HealthCheckController extends Controller
 {
     public function __invoke(Request $request, ResultStore $resultStore): Response
     {
+        $request->validate([
+
+        ]);
+        $uuid = $request->header('X-Owner');
+        if (!$uuid || !is_string($uuid) || (preg_match('/^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/', $uuid) !== 1)) {
+            return response("Missing X-Owner header", Response::HTTP_BAD_REQUEST);
+        }
+
         Artisan::call(RunHealthChecksCommand::class);
 
         $response = [
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HealthCheckController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
